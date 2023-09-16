41abac5914e9a904f5bb28f755a5e2a12a95eb44
Bhotton25 <bhotton25@gmail.com>
2023-09-16 15:23:26 -0600

    Completed Parts 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,85 @@ class Program
 {
     static void Main(string[] args)
     {
+        string percent = null;
+        string grade = null;
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your percentage? ");
+        percent = Console.ReadLine();
+        int num = int.Parse(percent);
+
+        //Grading System, starting with A Grade
+        if (num >= 90)
+        {
+            if (num >= 94)
+            {
+                grade = "A";
+            }
+            else
+            {
+                grade = "A-";
+            }
+        }
+        //B Grade
+        else if (num <= 89 && num >= 80)
+        {
+            if (num >= 87)
+            {
+                grade = "B+";
+            }
+            else if (num >= 84 && num < 87)
+            {
+                grade = "B";
+            }
+            else
+            {
+                grade = "B-";
+            }
+        }
+        //C Grade
+        else if (num <= 79 && num >= 70)
+        {
+            if (num >= 77)
+            {
+                grade = "C+";
+            }
+            else if (num >= 74 && num < 77)
+            {
+                grade = "C";
+            }
+            else
+            {
+                grade = "C-";
+            }
+        }
+        //D Grade
+        else if (num <= 69 && num >= 60)
+        {
+            if (num >= 67)
+            {
+                grade = "D+";
+            }
+            else if (num >= 64 && num < 67)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "D-";
+            }
+        }
+        //F Grade
+        else if (num < 60)
+        {
+            grade = "F";
+        }
+
+    //Printing Results    
+    if(num>=70){
+        Console.WriteLine($"You have a {num}%, which is a {grade}! Great Job!");
+    }else{
+        Console.WriteLine($"You have a {num}%, which is a {grade}. Try again, you can do this!");
+    }
     }
 }
