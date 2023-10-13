b011d25608718aa4a6e66cb1a612a18f6859b567
Bhotton25 <bhotton25@gmail.com>
2023-10-12 21:01:26 -0600

    Completed Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // 1/1 Fraction
+        Fraction first = new Fraction();
+        Console.WriteLine();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+
+        //Whole Fraction
+        Fraction second = new Fraction(5);
+        Console.WriteLine();
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+
+        //Different fraction
+        Fraction third = new Fraction(6, 8);
+        Console.WriteLine();
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
+        // 1/3 fraction
+        Fraction fourth = new Fraction(1, 3);
+        Console.WriteLine();
+        Console.WriteLine(fourth.GetFractionString());
+        Console.WriteLine(fourth.GetDecimalValue());
     }
 }
