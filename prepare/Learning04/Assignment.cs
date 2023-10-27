aea6b4391e32b09fe28068f4ae6f01f09f46865f
Bhotton25 <bhotton25@gmail.com>
2023-10-26 21:11:25 -0600

    finished learning activity

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -2,7 +2,7 @@ public class Assignment{
     private string _studentName;
     private string _topic;
 
-    publci Assignment(string studentName, string topic){
+    public Assignment(string studentName, string topic){
         _studentName = studentName;
         _topic = topic;
     }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Braxton Hotton", "C# Programming");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math = new MathAssignment("Braxton Hotton", "Multiplication", "5.2", "1-5");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("Braxton Hotton", "US History", "Rise of Technology");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
