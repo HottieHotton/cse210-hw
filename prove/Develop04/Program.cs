c6abc3912e11acfb9d6a86e1ddf4c9815b4954d5
Bhotton25 <bhotton25@gmail.com>
2023-11-02 20:00:05 -0600

    Added basic files, started template for how I want to manage program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string choice;
+        bool cont = true;
+
+        while(cont == true){
+            Console.WriteLine("Welcome to the Mindfullness Program!\n");
+            Console.WriteLine("Choose one of the following options:\n1: Breathing Activity\n2: Reflection Activity\n3: Listing Activity\n4: Quit\n");
+
+            Console.WriteLine("You choose: ");
+            choice = Console.ReadLine();
+
+            switch(choice){
+                case "1": break;
+                case "2": break;
+                case "3": break;
+                case "4":
+                Console.WriteLine("Goodbye!");
+                cont = false;
+                break;
+            }
+        }
     }
 }
